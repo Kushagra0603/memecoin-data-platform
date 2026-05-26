4b275e48336ac6a63a75c023c9b70ddf10330704
Kushagra0603 <kushagrapathak94@gmail.com>
2026-05-27 01:59:10 +0530

    Implemented Azure Blob Storage ETL pipeline

---
+++ ingestion/upload_to_blob.py
@@ -0,0 +1,53 @@
+from azure.storage.blob import BlobServiceClient
+from datetime import datetime
+import os
+
+from config import CONNECTION_STRING, CONTAINER_NAME
+
+# ----------------------------------------
+# CONNECT TO AZURE STORAGE
+# ----------------------------------------
+
+blob_service_client = BlobServiceClient.from_connection_string(
+    CONNECTION_STRING
+)
+
+# ----------------------------------------
+# LOCAL FILE PATH
+# ----------------------------------------
+local_file_path = (
+    "data/raw/memecoin_data_2026-05-25.csv"
+)
+
+# ----------------------------------------
+# BLOB STORAGE PATH
+# ----------------------------------------
+
+today = datetime.now()
+
+blob_path = (
+    f"coingecko/"
+    f"year={today.year}/"
+    f"month={today.month:02}/"
+    f"day={today.day:02}/"
+    f"memecoin_data_{today.date()}.csv"
+)
+
+# ----------------------------------------
+# CREATE BLOB CLIENT
+# ----------------------------------------
+
+blob_client = blob_service_client.get_blob_client(
+    container=CONTAINER_NAME,
+    blob=blob_path
+)
+
+# ----------------------------------------
+# UPLOAD FILE
+# ----------------------------------------
+
+with open(local_file_path, "rb") as data:
+    blob_client.upload_blob(data, overwrite=True)
+
+print("File uploaded successfully!")
+print(f"Blob Path: {blob_path}")
